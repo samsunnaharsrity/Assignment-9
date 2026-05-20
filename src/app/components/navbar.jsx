26a29cf3d22a8d7bca0dc00d23b1d0770e50762a
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-05-21 02:19:27 +0600

    added logout btn and responsive

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -1,8 +1,11 @@
 "use client"
 
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FiLogOut } from "react-icons/fi";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { IoMdCloseCircleOutline } from "react-icons/io";
 
@@ -11,6 +14,13 @@ const pathName = usePathname()
 // console.log(pathName);
 
 // const isActive = href === pathName;
+
+
+const { data: session } = authClient.useSession()
+//console.log(session);
+const user = session?.user;
+console.log(user);
+
 const navLinkClass = (path) =>
   pathName === path
     ? "font-medium text-green-800 border-b-2 border-green-800 pb-1"
@@ -80,7 +90,45 @@ useEffect(() => {
                         <Link href="/"
                         className={navLinkClass("/")}
                         >About</Link>
+                    {/* login logout register btn */}
+{
+      user? (
+      <div className="flex items-center gap-1">
+          <Avatar className="w-[25px] h-[25px] ">
+          <Avatar.Image alt="user img"
+          src={user?.image} />
+          <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+        </Avatar>
+
+
+            <div 
+            onClick={async() => await authClient.signOut()} 
+            className=" font-medium text-[12px] border rounded-sm py-1 px-2 text-white bg-green-800 hover:bg-green-600">
+                <Link href={"/"} className="flex items-center gap-1 font-semibold">
+                <span><FiLogOut className="text-[8px]"/></span>
+                Logout
+                </Link>
+            </div>
+
+      </div>)            
+        :         
+            (   <div className=" items-center gap-4">
+                    <div className="flex flex-col gap-2">
+
+                        <Link href="/login"
+                        className="font-medium text-[10px] text-gray-600 border border-gray-600 rounded-sm py-1 px-3 hover:bg-green-800 hover:text-white tracking-colors">
+                        Login
+                        </Link>
+
+                        <Link href="/register"
+                        className="font-medium text-[10px]  rounded-sm py-1 px-2 bg-green-800 text-white hover:bg-green-500">
+                        Register
+                        </Link>
                     </div>
+                </div>)
+}
+                    </div>
+
 
                     </div>
 
@@ -124,20 +172,44 @@ useEffect(() => {
 
                 </div>
 
-                <div className="hidden md:flex items-center gap-4">
-                    <div className="gap-2 flex">
+                {/* login logout register btn */}
+                {
+                    user? (
+                    <div className="flex items-center gap-1">
+                        <Avatar className="w-[25px] h-[25px] ">
+                        <Avatar.Image alt="user img"
+                        src={user?.image} />
+                        <Avatar.Fallback>{user.name.charAt(0)}</Avatar.Fallback>
+                        </Avatar>
 
-                        <Link href="/login"
-                        className="font-medium text-[14px] text-gray-600 rounded-sm py-1 px-2 border hover:text-green-800 hover:border border-green-800 tracking-colors">
-                        Login
-                        </Link>
 
-                        <Link href="/register"
-                        className="font-medium text-[14px] border rounded-sm py-1 px-2 bg-green-800 text-white hover:bg-green-500">
-                        Register
-                        </Link>
-                    </div>
-                </div>
+                            <div 
+                            onClick={async() => await authClient.signOut()} 
+                            className=" font-medium text-[12px] border rounded-sm py-1 px-2 text-white bg-green-800 hover:bg-green-600">
+                                <Link href={"/"} className="flex items-center gap-1 font-semibold">
+                                <span><FiLogOut className="text-[8px]"/></span>
+                                Logout
+                                </Link>
+                            </div>
+
+                    </div>)            
+                        :         
+                            (   <div className="hidden md:flex items-center gap-4">
+                                    <div className="gap-2 flex">
+
+                                        <Link href="/login"
+                                        className="font-medium text-[13px] text-gray-600 border border-gray-600 rounded-sm py-1 px-3 hover:bg-green-800 hover:text-white tracking-colors">
+                                        Login
+                                        </Link>
+
+                                        <Link href="/register"
+                                        className="font-medium text-[14px] border rounded-sm py-1 px-2 bg-green-800 text-white hover:bg-green-500">
+                                        Register
+                                        </Link>
+                                    </div>
+                                </div>)
+                }
+
             </div>
              
             </div>
