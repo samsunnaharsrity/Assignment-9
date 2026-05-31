a82c15ec96f14c675385a679b440b889764098c3
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-06-01 02:05:50 +0600

    trustedOrigins added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -24,6 +24,12 @@ const db = client.db("StudyNook");
 export const auth = betterAuth({
   database: mongodbAdapter(db),
 
+
+    trustedOrigins: [
+    "http://localhost:3000",
+    "https://studynook-iota.vercel.app",
+  ],
+
   emailAndPassword: {
     enabled: true,
   },
